668c91366cb8cee7765dcb90ffb89ec457311a77
Florian Duclos <florianduclos30@gmail.com>
2021-04-01 20:06:08 +0200

    authentification

---
+++ back/app/config/auth.config.js
@@ -0,0 +1,3 @@
+module.exports = {
+    secret: "aroundme-secret-key"
+  };
--- back/server.js
+++ back/server.js
@@ -10,6 +10,14 @@ var corsOptions = {
 
 app.use(cors(corsOptions));
 
+const db = require("./app/models");
+const Role = db.role;
+
+db.sequelize.sync({force: true}).then(() => {
+  console.log('Drop and Resync Db');
+  initial();
+});
+
 // parse requests of content-type - application/json
 app.use(bodyParser.json());
 
@@ -21,8 +29,29 @@ app.get("/", (req, res) => {
     res.json({ message: "Welcome to AroundMe&co application." });
 });
 
+// routes
+require('./app/routes/auth.routes')(app);
+require('./app/routes/user.routes')(app);
+
 // set port, listen for requests
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}.`);
 });
+
+function initial() {
+    Role.create({
+      id: 1,
+      name: "user"
+    });
+   
+    Role.create({
+      id: 2,
+      name: "moderator"
+    });
+   
+    Role.create({
+      id: 3,
+      name: "admin"
+    });
+}
